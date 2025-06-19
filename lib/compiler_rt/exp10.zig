4c77a24984e004aa4bf7defa60a092a3b5b15661
Christophe Delage <christ@ophe.net>
2025-06-19 10:47:06 +0200

    port exp10 from musl

--- lib/compiler_rt/exp10.zig
+++ lib/compiler_rt/exp10.zig
@@ -1,3 +1,10 @@
+// f32 and f64 functions ported from
+// musl/src/math/exp10f.c and
+// musl/src/math/exp10.c
+
+const std = @import("std");
+const math = std.math;
+
 const common = @import("common.zig");
 
 comptime {
@@ -12,29 +19,66 @@ comptime {
     @export(&exp10l, .{ .name = "exp10l", .linkage = common.linkage, .visibility = common.visibility });
 }
 
+const log2_10 = 3.3219280948873623478703194294893901758648313930246;
+
 fn __exp10h(x: f16) callconv(.c) f16 {
-    _ = x;
-    return 0;
+    // TODO: more efficient implementation
+    return @floatCast(exp10f(x));
 }
 
 fn exp10f(x: f32) callconv(.c) f32 {
-    _ = x;
-    return 0;
+    const p10 = [_]f32{ //
+        1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1,
+        1e1,  1e2,  1e3,  1e4,  1e5,  1e6,  1e7,
+    };
+
+    const ny = math.modf(x);
+    const n = ny.ipart;
+    const y = ny.fpart;
+    const m: i32 = @intFromFloat(n);
+    const i: u32 = @bitCast(n);
+    // fabsf(n) < 8 without raising invalid on nan
+    if (((i >> 23) & 0xff) < 0x7f + 3) {
+        const p = p10[@intCast(m + 7)];
+        if (y == 0) return p;
+        return @exp2(log2_10 * y) * p;
+    }
+
+    const z: f64 = x;
+    return @floatCast(@exp2(log2_10 * z));
 }
 
 fn exp10(x: f64) callconv(.c) f64 {
-    _ = x;
-    return 0;
+    const p10 = [_]f32{ //
+        1e-15, 1e-14, 1e-13, 1e-12, 1e-11, 1e-10, 1e-9, 1e-8, 1e-7,
+        1e-6,  1e-5,  1e-4,  1e-3,  1e-2,  1e-1,  1,    1e1,  1e2,
+        1e3,   1e4,   1e5,   1e6,   1e7,   1e8,   1e9,  1e10, 1e11,
+        1e12,  1e13,  1e14,  1e15,
+    };
+
+    const ny = math.modf(x);
+    const n = ny.ipart;
+    const y = ny.fpart;
+    const m: i64 = @intFromFloat(n);
+    const i: u64 = @bitCast(n);
+
+    // fabs(n) < 16 without raising invalid on nan
+    if (((i >> 52) & 0x7ff) < 0x3ff + 4) {
+        const p = p10[@intCast(m + 15)];
+        if (y == 0) return p;
+        return @exp2(log2_10 * y) * p;
+    }
+    return math.pow(f64, 10.0, x);
 }
 
 fn __exp10x(x: f80) callconv(.c) f80 {
-    _ = x;
-    return 0;
+    // TODO: more efficient implementation
+    return @floatCast(exp10q(x));
 }
 
 fn exp10q(x: f128) callconv(.c) f128 {
-    _ = x;
-    return 0;
+    // TODO: more correct implementation
+    return exp10(@floatCast(x));
 }
 
 fn exp10l(x: c_longdouble) callconv(.c) c_longdouble {
@@ -48,3 +92,29 @@ fn exp10l(x: c_longdouble) callconv(.c) c_longdouble {
     }
 }
 
+const expect = std.testing.expect;
+const expectRel = std.testing.expectApproxEqRel;
+
+test exp10f {
+    const eps = 50 * math.floatEps(f32);
+
+    try expect(exp10f(0) == 1);
+
+    try expectRel(1.584893193, exp10f(0.2), eps);
+    try expectRel(7.81627805, exp10f(0.893), eps);
+    try expectRel(31.6227766, exp10f(1.5), eps);
+    try expectRel(2.8183828546771552e+37, exp10f(37.45), eps);
+    try expectRel(0.1, exp10f(-1), eps);
+}
+
+test exp10 {
+    const eps = 50 * math.floatEps(f32);
+
+    try expect(exp10(0) == 1);
+
+    try expectRel(1.584893193, exp10(0.2), eps);
+    try expectRel(7.81627805, exp10(0.893), eps);
+    try expectRel(31.6227766, exp10(1.5), eps);
+    try expectRel(2.8183828546771552e+37, exp10(37.45), eps);
+    try expectRel(0.1, exp10(-1), eps);
+}
